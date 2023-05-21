0ce9eb62ff1d1b3f10375c3220d8fdf957f3ab1a
romeo <romeo.dark@mail.ru>
2023-05-21 12:50:17 +0300

    to output templates(Queryset)

--- crm/models.py
+++ crm/models.py
@@ -3,5 +3,12 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_name = models.CharField(max_length=200, verbose_name='Name')
+    order_phone = models.CharField(max_length=200, verbose_name='Phone')
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
